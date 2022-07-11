be1d62568ebb16a39d482aec3a489088856f2c4b
Camila Wolter <camila0wolter@gmail.com>
2022-07-11 16:14:06 -0300

    requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,15 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItemUrl = (id) => `https://api.mercadolibre.com/items/${id}`;
+
+const fetchItem = async (id) => {
+  try {
+    const url = fetchItemUrl(id);
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,23 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Deve ser uma função', () => {
+    expect(typeof(fetchItem)).toEqual('function');
+  });
+  it('O argumento "MLB1615760527" deve chamar a função fetch com o endpoint correto', async () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527');
+
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('O retorno da função fetchItem com o argumento "MLB1615760527" é igual ao objeto item', async () => {
+    const response = await fetchItem('MLB1615760527');
+
+    expect(response).toEqual(item);
+  });
+  it('Se a função fetchItem estiver sem argumento, deve retornar a mensagem de erro esperada', async () => {
+    const response = await fetchItem();
+
+    expect(response).toEqual(new Error('You must provide an url'));
+  });
 });
