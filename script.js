e737a3cf3c6f5bc6b1328887ac297f3ffb4d98dd
Camila Wolter <camila0wolter@gmail.com>
2022-07-11 18:19:18 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -12,6 +12,30 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+const cartItemClickListener = (event) => {
+  event.target.remove();
+};
+
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+async function fetchItemsResult(id) {
+  const result = await fetchItem(id);
+  const cartItems = document.querySelector('.cart__items');
+  const cart = createCartItemElement({ 
+    sku: result.id, name: result.title, salePrice: result.price });
+  
+  cartItems.appendChild(cart);
+  saveCartItems(cartItems.innerHTML);
+}
+
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -28,20 +52,6 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-const cartItemClickListener = (event) => {
-  // coloque seu código aqui
-};
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-};
-
 async function fetchProductsResult() {
   const itemsSection = document.getElementsByClassName('items')[0];
 
@@ -54,16 +64,7 @@ async function fetchProductsResult() {
       createProductItemElement({ sku: item.id, name: item.title, image: item.thumbnail }),
       ));
   }, 2000);
-};
-
-async function fetchItemsResult(id) {
-  const result = await fetchItem(id);
-  const cartItems = document.querySelector('.cart__items');
-  const cart = createCartItemElement({sku: result.id, name: result.title, salePrice: result.price});
-  
-  cartItems.appendChild(cart);
-  saveCartItems(cartItems.innerHTML);
-};
+}
 
 window.onload = () => {
   fetchProductsResult();
